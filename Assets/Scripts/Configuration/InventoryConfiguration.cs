bf6c3b827f37e8becb17c144138bf2955ebf7121
Santiago Dopazo Hilario <elmundoesazul@gmail.com>
2025-09-29 19:35:47 +0200

    Add InventoryConfiguration with system constants

---
+++ Assets/Scripts/Configuration/InventoryConfiguration.cs
@@ -0,0 +1,13 @@
+namespace InventorySystem.Configuration
+{
+    public static class InventoryConfiguration
+    {
+        public const int MaxInventorySize = 10;
+        public const int MaxStackSizeKey = 1;
+        public const int MaxStackSizeMoney = 999;
+        public const int MaxStackSizeApple = 10;
+        public const float DefaultItemValue = 1.0f;
+        public const int PickupRange = 1;
+        public const int GiveRange = 1;
+    }
+}
